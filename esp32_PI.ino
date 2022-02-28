e732fc9cc20201acff0603dd8ea7e3a69d83316a
James Hughes <jhughes1010@gmail.com>
2022-02-28 02:34:27 -0700

    dox WIP

--- esp32_PI.ino
+++ esp32_PI.ino
@@ -1,8 +1,33 @@
+/**
+* @file
+* @author James Hughes <jhughes1010@gmail.com>
+* @version 1.0 *
+* @section LICENSE *
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version. *
+* @section DESCRIPTION *
+* ESP32 verstion of the George Overton Arduino_PI project */
+/**
+* @brief ESP32 PI metal detector
+* @author James Hughes
+*/
+
+/** * * * *
+Standard Arduino setup().
+* - Initialize GPIOs
+* - Send LCD title
+* - Define TIMER3 interrupts
+*/
 void setup() {
   // put your setup code here, to run once:
 
 }
 
+/** * * * *
+Standard Arduino main loop()
+*/
 void loop() {
   // put your main code here, to run repeatedly:
 
